63ee7bf08dbce0e9a2ed30efce4e96e287fad88c
taka-kon <camenaide@gmail.com>
2020-08-10 19:43:02 +0900

    テンプレートkakeibotemp.blade.phpから継承させた

--- resources/views/join/index.blade.php
+++ resources/views/join/index.blade.php
@@ -1,38 +1,10 @@
-<!DOCTYPE html>
-	<html lang="ja">
-	<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" href="{{asset('css/style.css')}}">
-	<link href="https://fonts.googleapis.com/css?family=M+PLUS+Rounded+1c" rel="stylesheet">
-	<!-- font Awesomeを使用 -->
-	<link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
-	<!-- Chart.jsを使用する -->
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/1.0.2/Chart.min.js" type="text/javascript"></script>
-	<!-- jQueryを使用 -->
-	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
-	
-	
-	
-	<title>家計簿</title>
-	</head>
-	<body class="bg-input">
-	
-	<header class="header">
-	<div class="container">
-	<h1 class="header__title">家計簿アプリ</h1>
-	
-	<div class="header__button">
-	<a class="button button--logout" href="logout.php">ログアウト</a>
-	</div>
-	
-	
-	</div>
-	
-	</header>
+<!-- 新規登録（入力ページ） -->
+@extends('layouts.kakeibotemp')
 
+@section('title','登録｜')
 
-  <div class="main-input">
+@section('main')
+<div class="main-input">
     <h2 class="main-input__title">新規登録</h2>
     
 
@@ -90,5 +62,5 @@
 
   <a href="login.php">ログインページへ</a>
   </div>
-</body>
-</html>
+
+@endsection
