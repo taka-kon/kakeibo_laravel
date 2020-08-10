9b473f6d5749544f7c539f9a6ed5a3ad69fc224d
taka-kon <camenaide@gmail.com>
2020-08-11 00:04:41 +0900

    フォームリクエストを使ったpostメソッドを追加しました

--- app/Http/Controllers/JoinController.php
+++ app/Http/Controllers/JoinController.php
@@ -3,10 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\SignupRequest;
 
 class JoinController extends Controller
 {
-    public function index(){
-        return view('join.index');
+    public function index(Request $request){
+        return view('join.index',['msg'=>'フォームを入力']);
+    }
+
+    public function post(SignupRequest $request){
+        return view('join.index',['msg'=>'正しく入力されました！']);
     }
 }
