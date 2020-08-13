cdbcd70edaaf74efb6e25577d8865a7855e59e28
taka-kon <camenaide@gmail.com>
2020-08-13 11:55:10 +0900

    エラー条件を増やしました

--- app/Http/Requests/SignupRequest.php
+++ app/Http/Requests/SignupRequest.php
@@ -30,18 +30,26 @@ class SignupRequest extends FormRequest
         return [
             'name' => 'required',
             'email' => 'required|email',
-            'password' => 'required|min:6'
+            'password' => 'required|min:6',
+            'sex' => 'required',
+            'month'=>'required',
+            'year'=>'required',
+            'month'=>'required',
+            'day'=>'required',
         ];
     }
 
     public function messages(){
         return [
             'name.required' => '* ニックネームが入力されていません。',
-            'email.required' => 'メールアドレスが入力されていません。',
-            'email.email' => 'メールアドレスを入力してください',
-            'email.email' => 'メールアドレスを入力してください',
-            'password.required' => 'パスワードを入力してください',
-            'password.min' => '文字数が少なすぎます。',
+            'email.required' => '* メールアドレスが入力されていません。',
+            'email.email' => '* メールアドレスを入力してください',
+            'password.required' => '* パスワードを入力してください',
+            'password.min' => '* 文字数が少なすぎます。',
+            'sex.required' => '* 性別を選択してください。',
+            'year.required'=>"* 年を選択してください。",
+            'month.required'=>"* 月を選択してください。",
+            'day.required'=>"* 日を選択してください。",
         ];
     }
 }
